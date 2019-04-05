816ccb00ec3ff785caabafecfcf0c3b73fdf4259
Darrin Skinner <skinner@skinner.com>
2019-04-05 12:36:26 -0700

    use non SSL connection class when port is not  5671

--- src/Request.php
+++ src/Request.php
@@ -3,6 +3,7 @@
 use Illuminate\Config\Repository;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Connection\AMQPSSLConnection;
+use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Channel\AMQPChannel;
 
 /**
@@ -31,15 +32,25 @@ class Request extends Context
      */
     public function connect()
     {
-        $this->connection = new AMQPSSLConnection(
-            $this->getProperty('host'),
-            $this->getProperty('port'),
-            $this->getProperty('username'),
-            $this->getProperty('password'),
-            $this->getProperty('vhost'),
-            $this->getProperty('ssl_options'),
-            $this->getProperty('connect_options')
-        );
+        if ($this->getProperties('port') == 5671) {
+            $this->connection = new AMQPSSLConnection(
+                $this->getProperty('host'),
+                $this->getProperty('port'),
+                $this->getProperty('username'),
+                $this->getProperty('password'),
+                $this->getProperty('vhost'),
+                $this->getProperty('ssl_options'),
+                $this->getProperty('connect_options')
+            );
+        } else {
+            $this->connection = new AMQPConnection(
+                $this->getProperty('host'),
+                $this->getProperty('port'),
+                $this->getProperty('username'),
+                $this->getProperty('password'),
+                $this->getProperty('vhost')
+            );
+        }
 
         $this->channel = $this->connection->channel();
     }
